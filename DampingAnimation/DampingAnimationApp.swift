90241cd92a64c29b97dda741de35abefd9b204ea
rahulon12 <rahulon@icloud.com>
2022-04-03 18:26:42 -0400

    Initial Commit

---
+++ DampingAnimation/DampingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DampingAnimationApp.swift
+//  DampingAnimation
+//
+//  Created by Rahul Narayanan on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DampingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
